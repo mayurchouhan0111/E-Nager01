c116fb063eda4152c40bfb83dc9f10aa3a5a7985
mayur <mayurchouhan8055@gmail.com>
2026-08-12 00:42:21 +0530

    Fix: Add missing subscribeNotifications import in ServiceHeader.js

--- src/components/ServiceHeader.js
+++ src/components/ServiceHeader.js
@@ -2,7 +2,7 @@
 import React, { useState, useEffect } from 'react';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { getNotifications, markNotificationAsRead } from '../services/notificationService';
+import { getNotifications, markNotificationAsRead, subscribeNotifications } from '../services/notificationService';
 import { getBirthCertificates } from '../services/birthCertificateService';
 import { getDeathCertificates } from '../services/deathCertificateService';
 import { getWaterConnections } from '../services/waterConnectionService';
